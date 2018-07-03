1c6ea1c4e5000ed2976a452bdb5d9962353c07eb
Darko Mijatovic <dmijato1@gmail.com>
2018-07-03 14:46:04 -0400

    added created new task test

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -6,21 +6,29 @@ RSpec.feature "Tasks", type: :feature do
     @user = create(:user)
     create(:task)
     login_user(@user)
+    click_on(@user.email)
   end
   
   scenario 'User views all tasks' do
-   click_on(@user.email)
    click_on('All Tasks')
 
    expect(page).to have_content('Foo Bar')
   end
 
-  scenario 'User views taks only belonging to him' do
-    click_on(@user.email)
+  scenario 'User views tasks only belonging to him' do
     click_on('All Tasks')
 
     expect(page).to have_content('Foo Bar')
   end
 
-  
+  scenario 'User creates a new task' do
+    click_on('New Task')
+    fill_in 'task_serial_no', with: '12345'
+    fill_in 'task_account', with: '1-2345-6'
+    find('#task_assigned_to').find(:xpath, 'option[1]').select_option
+    click_on('Submit')
+
+    expect(page).to have_content('Task created')
+    expect(Task.all.size).to eq 2
+  end
 end
