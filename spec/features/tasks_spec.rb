99364bcf188361ce565adf62096b3a24cec1aeb8
Darko Mijatovic <dmijato1@gmail.com>
2018-07-04 12:19:38 -0400

    added test for updating tasks

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -10,25 +10,35 @@ RSpec.feature "Tasks", type: :feature do
   end
   
   scenario 'User views all tasks' do
-   click_on('All Tasks')
+   click_on 'All Tasks'
 
    expect(page).to have_content('Foo Bar')
   end
 
   scenario 'User views tasks only belonging to him' do
-    click_on('All Tasks')
+    click_on 'All Tasks'
 
     expect(page).to have_content('Foo Bar')
   end
 
   scenario 'User creates a new task' do
-    click_on('New Task')
+    click_on 'New Task'
     fill_in 'task_serial_no', with: '12345'
     fill_in 'task_account', with: '1-2345-6'
     find('#task_assigned_to').find(:xpath, 'option[1]').select_option
-    click_on('Submit')
+    click_on 'Submit'
 
     expect(page).to have_content('Task created')
     expect(Task.all.size).to eq 2
+  end
+
+  scenario 'User updates a task' do
+    click_on 'All Tasks'
+    click_link 'Update'
+    fill_in 'task_serial_no', with: 'updated task'
+    click_on 'Update'
+
+    expect(page).to have_content('Task has been updated')
+    expect(page).to have_content('updated task')
   end
 end
