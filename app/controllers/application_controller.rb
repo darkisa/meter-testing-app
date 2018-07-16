6c6a8d136918dc9ab1dee49db5ef3cb17ffdf97c
Darko Mijatovic <dmijato1@gmail.com>
2018-07-16 12:09:02 -0400

    removed the ability to select a protocol from the the dropwdown since that will be done based on the test selected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :get_dropdown_variables
-
-  def get_dropdown_variables
-    @protocols = Protocol.all
-  end
-
 end
